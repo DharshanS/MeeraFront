9e667155ea54d434e05c03c651f4b879b8ae8ff3
sithamparam mirdula <sithamparam mirdula>
2017-05-22 01:17:23 +0530

    payment

--- frmpayment.php
+++ frmpayment.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/jquery-ui.css" />
-  <link rel="stylesheet" href="css/form.css" />
+  <link rel="stylesheet" href="css/payment.css" />
   
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
@@ -38,44 +38,174 @@
       <!--end navbar --> 
     </div>
 
-    <div class="container-fluid">
+    
+        
+    
+      
+    <div class="container">
+        <form action="" method="" id="frmoption">  
         <div class="row clsheadopt">
             <div class="col-lg-12"><h3>PAYMENT OPTION</h3></div>
             <div class="col-lg-12"><h5>TOTAL TO BE PAID IS LKR 37,577</h5></div>
         </div>
-    </div>    
-    <div class="container">    
-        <div class="row">
-            <div class="col-lg-12">
-            <div class="col-lg-3">
-               gbgbg 
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                
             </div>
-            <div class="col-lg-8">
-               bbgbgb 
+            <div class="col-lg-2">
+                <span class="clsdebit">CREDIT/DEBIT CARD </span>
+            </div>
+            <div class="col-lg-7">
+               
             </div>
             <div class="col-lg-1">
-                <input type="radio" name="paytype" id="">
-            </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 pull-right">
-                <div class="col-lg-6"><h5>TOTAL AMOUNT</h5></div>
-                <div class="col-lg-6"><h5>LKR 36,577</h5></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-8">
-            </div>
-            <div class="col-lg-2">
-                <div class="col-lg-12"><h5>BACK</h5></div>
-                
-            </div>
-            <div class="col-lg-2">
-                <div class="col-lg-12"><h5>MAKE PAYMENT</h5></div>
                 
             </div>
         </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                <img class="img-responsive" src="images/listicon.png">
+            </div>                
+           
+            <div class="col-lg-2 imgcls">
+                <img class="img-responsive" src="images/visa.png">
+            </div> 
+            <div class="col-lg-8 cardcls">
+                <span>We Accept All VISA Cards Issued by Sri Lankan Banks </span>
+            </div> 
+            <div class="col-lg-1 cardcls">
+                <span class="radioon"></span><input type="radio" name="visacard" id="visacard" >
+            </div>
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                <img class="img-responsive" src="images/listicon.png"> 
+            </div>
+            <div class="col-lg-2 imgcls">
+                <img class="img-responsive" src="images/master.png"/>
+            </div>
+            <div class="col-lg-8 cardcls">
+               <span>We Accept All MASTER Cards Issued by Sri Lankan Banks </span>
+            </div>
+            <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="mastercard" id="mastercard" >
+            </div>
+            
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                <img class="img-responsive" src="images/listicon.png"> 
+            </div>
+            <div class="col-lg-2 imgcls">
+                <img class="img-responsive" src="images/amarican.png"/>
+            </div>
+            <div class="col-lg-8 cardcls">
+               <span>We Accept All AMERICAN EXPRESS Cards Issued by Sri Lankan Banks </span>
+            </div>
+             <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="amaricancard" id="amaricancard" >
+            </div>
+            
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                 <img class="img-responsive" src="images/listicon.png">
+            </div>
+            <div class="col-lg-2 imgcls">
+                <img class="img-responsive" src="images/standerd.png"/>
+            </div>
+            <div class="col-lg-8 cardcls1">
+               <span>Standard Chartered Bank </span>
+            </div>
+             <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="standerdcard" id="standerdcard" >
+            </div>
+            
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                <img class="img-responsive" src="images/listicon.png"> 
+            </div>
+            <div class="col-lg-2 imgcls imgclsimg">
+                <img class="img-responsive" src="images/hnb.png"/>
+            </div>
+            <div class="col-lg-8 cardcls2">
+               <span>Hatton National Bank</span>
+            </div>
+             <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="hnbcard" id="hnbcard" >
+            </div>
+            
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                 <img class="img-responsive" src="images/listicon.png">
+            </div>
+            <div class="col-lg-2 imgcls imgclsimg">
+                <img class="img-responsive" src="images/commercial.png"/>
+            </div>
+            <div class="col-lg-8 cardcls2">
+               <span>Commercial Bank</span>
+            </div>
+             <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="commercialcard" id="commercialcard" >
+            </div>
+            
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                 <img class="img-responsive" src="images/listicon.png">
+            </div>
+            <div class="col-lg-2 imgcls">
+                <img class="img-responsive"  src="images/seylan.png"/>
+            </div>
+            <div class="col-lg-8 cardcls2">
+               <span>Seylan Bank </span>
+            </div>
+             <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="seylancard" id="seylancard" >
+            </div>
+        </div>
+        <div class="row payfirst">
+            <div class="col-lg-1">
+                 <img class="img-responsive" src="images/listicon.png">
+            </div>
+            <div class="col-lg-2 imgcls imgclsimg">
+                <img class="img-responsive"  src="images/sampath.png"/>
+            </div>
+            <div class="col-lg-8 cardcls2">
+               <span>Sampath Bank Cards</span>
+            </div>
+            <div class="col-lg-1 cardcls">
+                <span class="radiooff"></span><input type="radio" name="sampathcard" id="sampathcard" >
+            </div>
+            
+        </div>
+        <div class="row totmain">
+           
+                <div class="col-lg-7"></div>
+                <div class="col-lg-3"><label class="totcls">TOTAL AMOUNT -</label></div>
+                <div class="col-lg-2"><label class="totcls1">LKR 36,577</label></div>
+            
+        </div>
+        <div class="row">
+            <div class="col-lg-5">
+            </div>
+            <div class="col-lg-3 backbtncls" >
+                <div class="backbtn img-responsive"></div>
+                
+            </div>
+            <div class="col-lg-4 imgclsimg">
+                <div class="mkpayment img-responsive"></div>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 titlecls imgclsimg">
+                We use the highest secure payment gateway to process your transaction.
+            </div>    
+        </div>    
+    </form>
     </div>
 
     
